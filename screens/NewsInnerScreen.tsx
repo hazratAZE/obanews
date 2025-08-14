17f5dbae7248acbb9c7070478dd6cc83c2f7c9b1
qafulov <qafulovh@gmail.com>
2025-08-14 17:56:32 +0400

    Added news inner news from api

--- screens/NewsInnerScreen.tsx
+++ screens/NewsInnerScreen.tsx
@@ -1,13 +1,58 @@
 // screens/NewsInnerScreen.tsx
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../types/navigation';
+import { RootStackParamList, NewsItem } from '../types/navigation';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'NewsInner'>;
 
-const NewsInnerScreen: React.FC<Props> = ({ route, navigation }) => {
-  const { news } = route.params; // HomeScreen-dən göndəriləcək news object
+const NewsInnerScreen: React.FC<Props> = ({ route }) => {
+  const { news } = route.params; // HomeScreen-dən göndərilən news objesi
+  const [newsDetail, setNewsDetail] = useState<NewsItem | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNewsDetail = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${news.id}`,
+        );
+        const data = await response.json();
+        console.log(data);
+        setNewsDetail(data);
+      } catch (error) {
+        console.error('Xəbər detayı yüklənmədi:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNewsDetail();
+  }, [news.id]);
+
+  if (loading) {
+    return (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" color="#28a745" />
+        <Text>Yüklənir...</Text>
+      </View>
+    );
+  }
+
+  if (!newsDetail) {
+    return (
+      <View style={styles.loader}>
+        <Text>Xəbər tapılmadı!</Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -17,9 +62,8 @@ const NewsInnerScreen: React.FC<Props> = ({ route, navigation }) => {
         }}
         style={styles.image}
       />
-
-      <Text style={styles.title}>{news.title}</Text>
-      {news.body ? <Text style={styles.body}>{news.body}</Text> : null}
+      <Text style={styles.title}>{newsDetail.title}</Text>
+      <Text style={styles.body}>{newsDetail.body}</Text>
     </ScrollView>
   );
 };
@@ -29,13 +73,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f9f9f9',
   },
-  backButton: {
-    padding: 12,
-  },
-  backText: {
-    fontSize: 16,
-    color: '#28a745',
-    fontWeight: '600',
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   image: {
     width: '100%',
